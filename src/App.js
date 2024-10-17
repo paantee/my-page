8db097ee46fec9254cfaa055540bd16898893c2e
paantee <teemu-paananen@hotmail.com>
2024-10-17 16:45:12 +0300

    Opinnäytetyö aloitettu

--- src/App.js
+++ src/App.js
@@ -1,19 +1,13 @@
 import './App.css';
 import { Routes, Route, HashRouter } from "react-router-dom";
-import { MyPage } from './components/MyPage';
+import { MyPage } from './components/HomePage';
 import { ProjectsPage } from './components/ProjectsPage';
 import { Approuter } from './Approuter';
 
 function App() {
   return (
     <>
-      <HashRouter>
-        <Routes>
-          <Route path="/" element={<Approuter />}></Route>
-            <Route index path="/home" element={<MyPage />} />
-            <Route path="/projects" element={<ProjectsPage />} />
-        </Routes>
-      </HashRouter>
+      <Approuter></Approuter>
     </>
   );
 }
