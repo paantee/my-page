d4b1092159f5cfa0baa098b5ca31e80cc1336795
paantee <teemu-paananen@hotmail.com>
2024-10-21 17:45:40 +0300

    Home Page work in progress

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,3 +1,4 @@
+import { Container, Grid2 } from "@mui/material"
 
 export const HomePage = () => {
 
@@ -5,7 +6,21 @@ export const HomePage = () => {
   return (
     <>
       <div id="homeDiv">
-        <p>Home</p>
+        <Grid2 container>
+          <Grid2 size={6}>
+            <div style={{height: '500px'}}>
+              <h1 style={{textAlign: 'center', marginTop: '250px', fontSize: '50px'}}>
+                Teemu Paananen
+              </h1>
+              <p>Text</p>
+            </div>
+          </Grid2>
+          <Grid2 size={6}>
+            <div style={{height: '500px'}}>
+
+            </div>
+          </Grid2>
+        </Grid2>
       </div>
     </>
   )
